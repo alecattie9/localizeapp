6f19b1403dcc22be650282c1e4f5e11f28aab25e
Alec Attie <Alecattie13@gmail.com>
2018-01-15 17:51:35 -0600

    init

--- Localization app/ViewController.swift
+++ Localization app/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        let alert = UIAlertController(title: "welcome", message: "dd", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
