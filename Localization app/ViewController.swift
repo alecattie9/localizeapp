cd70584be16a4d32be51c0db83c4168561aff343
Alec Attie <Alecattie13@gmail.com>
2018-01-15 17:10:08 -0600

    Initial Commit

---
+++ Localization app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Localization app
+//
+//  Created by Alec Attie on 1/15/18.
+//  Copyright © 2018 Alec Attie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
